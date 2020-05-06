52d276db4faae2a486fe89c73957b33cb9a88da4
traczkowski <raczkowskitm@gmail.com>
2020-05-06 17:23:55 -0400

    Adding Week 5 PSQL

---
+++ 5_SQL_Querying_Alchemy/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,12 @@
+dbname = 'dvdrental'
+host = 'localhost'
+port = '5432'
+user = 'tomraczkowski'
+password = '404GTbees!!'
+
+
+prod_dbname = 'dvdrental'
+prod_host = 'localhost'
+prod_port = '5432'
+prod_user = 'tomraczkowski'
+prod_password = '404GTbees!!'
